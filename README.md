4ee4d75deb3dd337862f6dde834d858a7b414651
Mat Gargano <mgargano@gmail.com>
2015-09-23 14:26:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+HTTPCheck
+==================================================
+
+
+`Version 0.0.1`
+
+
+Release Notes
+---
+
+* 0.0.1
+Initial Release
+
+
+
+
---
+++ src/resource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace HTTPCheck;
+
+use GuzzleHttp\Client;
+
+
+class Resource {
+
+	private $client;
+	private $url;
+	private $method;
+
+	public function __construct( Client $client ) {
+
+
+		$this->client = $client;
+
+
+	}
+
+	private function grab() {
+
+		try {
+			return $this->client->request( $this->method, $this->url )->getStatusCode();
+		} catch ( \Exception $ex ) {
+
+			return $ex->getCode();
+		}
+
+	}
+
+	public function get_status_code( $url, $method = 'GET' ) {
+
+		$this->url    = $url;
+		$this->method = $method;
+
+		return $this->grab();
+
+
+	}
+
+
+}
